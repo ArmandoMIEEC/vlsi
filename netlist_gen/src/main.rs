af6ecba19aeba2404bac1c2ebbed177af0ec49a5
Armando <zearmando2000@gmail.com>
2022-05-21 00:15:04 +0100

    some  pattern gen added

--- netlist_gen/src/main.rs
+++ netlist_gen/src/main.rs
@@ -4,12 +4,27 @@ use std::io::prelude::*;
 
 fn main() {
     let mut a: [i32; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    let b: [i32; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let mut b: [i32; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
     for param in a.iter_mut() {
         *param = 1;
     }
 
+    let filenumber = 0;
+    let filename = "tb.scs";
+    let mut newfilename = format!("{}", filenumber);
+
+    gen_netlist(filename, newfilename.as_str(), &a, &b);
+
+    for filenumber in 1..15 {
+        b[filenumber] = 1;
+        newfilename = format!("{}", filenumber);
+        gen_netlist(filename, newfilename.as_str(), &a, &b);
+        b[filenumber] = 0;
+    }
+}
+
+fn gen_netlist(filename: &str, newfilename: &str, a: &[i32], b: &[i32]) {
     let mut newline = String::from("parameters ");
     let mut cnt = 0;
 
@@ -26,31 +41,23 @@ fn main() {
         cnt = cnt + 1;
     }
 
-    println!("{}", newline);
-
-    let filename = "/Users/armando/Documents/mac/my_dir/github/vlsi/netlist_gen/docs/tb.scs";
-    let file = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let mut new_file = File::create(
-        "/Users/armando/Documents/mac/my_dir/github/vlsi/netlist_gen/docs/tb_auto.scs",
-    )
-    .expect("Something went wrong creating the file");
+    let path = "/Users/armando/Documents/mac/my_dir/github/vlsi/netlist_gen/docs/";
+    let file = fs::read_to_string(format!("{}{}", path, filename))
+        .expect("Something went wrong reading the file");
+    let mut new_file = File::create(format!("{}{}", path, newfilename))
+        .expect("Something went wrong creating the file");
 
     let mut line_number = 0;
     for line in file.lines() {
         if line_number == 15 {
-            println!("Found line 16:");
-            println!("{}", line);
             new_file
                 .write_all(format!("{}\n", newline).as_bytes())
                 .expect("Something went wrong writing a line");
-            println!("Replaced line 16:");
-            println!("{}", newline);
         } else {
             new_file
                 .write_all(format!("{}\n", line).as_bytes())
                 .expect("Something went wrong writing a line");
         }
-
         line_number = line_number + 1;
     }
 }
